ca2b3a3c34c046dec6c668c76bbeb03e94a1ba34
jeremy <poohc300@gmail.com>
2022-05-25 13:42:55 +0900

    mysite #4 add web

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,29 +1,25 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.mysite.web.mvc.main.MainActionFactory;
-import com.douzone.web.mvc.Action;
-import com.douzone.web.mvc.ActionFactory;
 
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		String actionName = request.getParameter("a");
-		
-		ActionFactory factory = new MainActionFactory();
-		Action action = factory.getAction(actionName);
-		action.execute(request, response);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+
 }
